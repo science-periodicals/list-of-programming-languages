4ced17abc8883f54d0529eb55c1ed17c4e33b013
sballesteros <sebastien.ballesteros@gmail.com>
2019-07-08 10:42:22 -0700

    migrate to scipe

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # list-of-programming-languages
-The Wikipedia page ["List of programming languages"](https://en.wikipedia.org/wiki/List_of_programming_languages) converted into JSON-LD.
+
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+Note: this module is auto published to npm on CircleCI. Only run `npm version
+patch|minor|major` and let CI do the rest.
+
+The Wikipedia
+page
+["List of programming languages"](https://en.wikipedia.org/wiki/List_of_programming_languages) converted
+into JSON-LD.
+
+Note: this module is auto published to npm on CircleCI. Only run `npm version
+patch|minor|major` and let CI do the rest.
--- data/index.js
+++ data/index.js
@@ -19,22 +19,27 @@ request.get(lplUrl, function(err, resp, body) {
   var list = {
     '@context': 'http://schema.org',
     '@type': ['ItemList', 'CreativeWork'],
-    'inLanguage': 'English',
-    'description': $('#mw-content-text p').eq(0).text(),
-    'version': packageJson.version,
-    'dateModified': (new Date()).toISOString(),
-    'isBasedOnUrl': lplUrl,
-    'itemListOrder': 'schema:ItemListOrderAscending',
-    'numberOfItems': 0,
-    'itemListElement': [],
+    inLanguage: 'English',
+    description: $('#mw-content-text p')
+      .eq(0)
+      .text(),
+    version: packageJson.version,
+    dateModified: new Date().toISOString(),
+    isBasedOnUrl: lplUrl,
+    itemListOrder: 'schema:ItemListOrderAscending',
+    numberOfItems: 0,
+    itemListElement: []
   };
 
   $('h2 ~ .div-col li a').each(function(i) {
     var $a = $(this);
     list.itemListElement.push({
-      "@type": 'ListItem',
+      '@type': 'ListItem',
       item: {
-        '@id': url.resolve(plplUrl.protocol + '//' + plplUrl.host, $a.attr('href')),
+        '@id': url.resolve(
+          plplUrl.protocol + '//' + plplUrl.host,
+          $a.attr('href')
+        ),
         '@type': 'ComputerLanguage',
         name: $a.text()
       }
@@ -42,20 +47,25 @@ request.get(lplUrl, function(err, resp, body) {
   });
 
   list.itemListElement = uniqBy(
-    list.itemListElement.concat(ml.map(function(item) {
-      return {
-        "@type": 'ListItem',
-        item: {
-          '@id': item.url,
-          '@type': 'ComputerLanguage',
-          name: item.name
-        }
-      };
-    })).sort(function(a, b) {
-      return a.item.name.localeCompare(b.item.name);
-    }).map(function(itemListElement, i) {
-      return Object.assign(itemListElement, { position: i });
-    }),
+    list.itemListElement
+      .concat(
+        ml.map(function(item) {
+          return {
+            '@type': 'ListItem',
+            item: {
+              '@id': item.url,
+              '@type': 'ComputerLanguage',
+              name: item.name
+            }
+          };
+        })
+      )
+      .sort(function(a, b) {
+        return a.item.name.localeCompare(b.item.name);
+      })
+      .map(function(itemListElement, i) {
+        return Object.assign(itemListElement, { position: i });
+      }),
     function(itemListElement) {
       return itemListElement.item['@id'];
     }
